d6e20782acdf0df4336052dc05756a9e31bdc609
James Reed <jcrd@tuta.io>
2020-10-05 16:01:54 -0600

    menu: Spawn workspaces with wm-launch

--- src/menu.lua
+++ src/menu.lua
@@ -4,8 +4,11 @@ local workspace = require('awesome-launch.workspace')
 
 local menu = {}
 
+local env = os.getenv('WM_LAUNCH_WORKSPACE_FILENAME')
+
 menu.workspace = {}
 menu.workspace.search_paths = {}
+menu.workspace.filename = env or '.workspace'
 
 function menu.workspace.new()
     if #menu.workspace.search_paths == 0 then
@@ -13,18 +16,13 @@ function menu.workspace.new()
     end
     local paths = table.concat(menu.workspace.search_paths, ' ')
     local cmd = string.format('find %s -maxdepth 2 -name %s', paths,
-        workspace.filename)
+        menu.workspace.filename)
     cmd = cmd..' -printf "%h\n" | rofi -dmenu -p workspace'
     awful.spawn.easy_async_with_shell(cmd, function (out)
         out = string.gsub(out, '\n', '')
-        if out == '' then
-            return
+        if out ~= '' then
+            awful.spawn('wm-launch -w '..out)
         end
-        local t = workspace.new(out:match('([^/]+)$'), {
-            props = {layout = awful.layout.layouts[1]},
-            load_workspace = out,
-        })
-        t:view_only()
     end)
 end
 
