d020b07ddc0008b816001aaa5b6f236508f9e721
James Reed <jcrd@tuta.io>
2020-12-18 15:53:59 -0700

    notif: Require battery widget only if enabled

--- src/notification.lua
+++ src/notification.lua
@@ -2,7 +2,6 @@ local naughty = require('naughty')
 
 local session = require('sessiond_dbus')
 
-local battery = require('dovetail.widgets.battery')
 local config = require('dovetail.config')
 local inhibit = require('dovetail.inhibit')
 
@@ -26,35 +25,39 @@ session.on_backlight_error = function (msg)
     }
 end
 
-local batt_charged = false
-local batt_low = false
+if config.options.enable_battery_widget then
+    local battery = require('dovetail.widgets.battery')
 
-battery.on_update = function (power, time, percent)
-    if power then
-        batt_low = false
-        if percent >= config.options.battery_charged_percent then
-            if batt_charged then
-                return
+    local batt_charged = false
+    local batt_low = false
+
+    battery.on_update = function (power, time, percent)
+        if power then
+            batt_low = false
+            if percent >= config.options.battery_charged_percent then
+                if batt_charged then
+                    return
+                end
+                naughty.notification {
+                    urgency = 'normal',
+                    title = string.format('Full battery (%d%%)', percent),
+                    message = 'Battery is charged',
+                }
+                batt_charged = true
             end
-            naughty.notification {
-                urgency = 'normal',
-                title = string.format('Full battery (%d%%)', percent),
-                message = 'Battery is charged',
-            }
-            batt_charged = true
-        end
-    else
-        batt_charged = false
-        if percent <= config.options.battery_low_percent then
-            if batt_low then
-                return
+        else
+            batt_charged = false
+            if percent <= config.options.battery_low_percent then
+                if batt_low then
+                    return
+                end
+                naughty.notification {
+                    urgency = 'critical',
+                    title = string.format('Low battery (%d%%)', percent),
+                    message = string.format('Time to empty: %s', time),
+                }
+                batt_low = true
             end
-            naughty.notification {
-                urgency = 'critical',
-                title = string.format('Low battery (%d%%)', percent),
-                message = string.format('Time to empty: %s', time),
-            }
-            batt_low = true
         end
     end
 end
